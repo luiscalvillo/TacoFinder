b24631596ca5c132d86eadf22717b1036180b1eb
Luis Calvillo <luiscalvillo89@gmail.com>
2020-12-29 02:22:39 -0800

    Added MapViewController

---
+++ TacoFinder/Controller/MapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MapViewController.swift
+//  TacoFinder
+//
+//  Created by Luis Calvillo on 12/29/20.
+//  Copyright © 2020 Luis Calvillo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
